b445d64fa14f37b0da097d531fe5aee648b8bc4b
Guillermo Mainini <guimainini@gmail.com>
2021-03-08 23:37:29 -0300

    cree un app.get para poder desplegar a produccion3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -26,9 +26,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, {
-    useHash: false
-  })
+    RouterModule.forRoot(routes)
 ],
   exports: [RouterModule]
 })
